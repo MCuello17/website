ef93e99b18fe64b04371209a816858b67090d25c
Michael Cuello <mjcuello17@gmail.com>
2020-12-23 19:34:55 -0300

    setup screen

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap');
+
+* {
+  box-sizing: border-box;
+  font-family: 'Roboto Mono', monospace;
+  font-weight: 700;
+  font-size: 20px;
+  user-select: none;
+}
+
+:root {
+  --primary: #24c915;
+  --background: #121d2c;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +32,26 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.cut-corner {
+    position: relative;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.cut-corner:before {
+    position: absolute;
+    left: -1px;
+    bottom: -1px;
+    content: '';
+    border-bottom: 70px solid;
+    border-color: inherit;
+    border-right: 70px solid transparent;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.cut-corner:after {
+    position: absolute;
+    left: -3px;
+    bottom: -3px;
+    content: '';
+    border-bottom: 70px solid var(--background);
+    border-right: 70px solid transparent;
 }
 </style>
---
+++ src/components/MainComputer.vue
@@ -0,0 +1,111 @@
+<template>
+  <main :id="mobile ? 'mobile-view' : 'desktop-view'">
+      <section id="screen" class="cut-corner">
+          <h1 class="title">
+              Test Title
+          </h1>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+          <div class="icon"></div>
+      </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainComputer',
+  data () {
+    return {
+      mobile: window.innerWidth <= 992
+    }
+  },
+  created () {
+    addEventListener('resize', () => {
+      this.mobile = innerWidth <= 992
+    })
+  }
+}
+</script>
+
+<style>
+    #mobile-view,
+    #desktop-view {
+        height: 100vh;
+        width: 100vw;
+        overflow: hidden;
+        background-color: var(--background);
+        color: var(--primary);
+        padding: 25px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #screen {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        flex-grow: 1;
+        border: 2px solid var(--primary);
+        padding: 20px;
+        display: grid;
+        grid-gap: 5px;
+        justify-content: center;
+        grid-template-columns: repeat(auto-fill, 90px);
+        grid-auto-rows: 90px;
+    }
+
+    #screen:before,
+    #screen:after {
+        position: fixed;
+    }
+
+    #screen:before {
+        bottom: 27px;
+        left: 27px;
+    }
+
+    #screen:after {
+        bottom: 25px;
+        left: 25px;
+    }
+
+    #desktop-view #screen {
+        overflow: hidden;
+    }
+
+    #mobile-view #screen {
+        overflow-y: scroll;
+    }
+
+    .title {
+        grid-column: 1 / -1;
+    }
+
+    .icon {
+        border: 1px solid #fff;
+    }
+</style>
